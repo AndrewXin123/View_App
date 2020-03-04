ce2bbbdfb688efb43e2f524ad3037ff34d9ce231
AndrewXin123 <AndrewXin12345@gmail.com>
2020-03-04 13:06:34 -0500

    Finished

--- app/src/main/java/com/zhangandrew/viewapp/MainActivity.java
+++ app/src/main/java/com/zhangandrew/viewapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.zhangandrew.viewapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.one);
+    }
+    public void onClick1(View view){
+        setContentView(R.layout.two);
+    }
+    public void onClick2(View view){
+        setContentView(R.layout.three);
+    }
+    public void onClick3(View view){
+        setContentView(R.layout.four);
+    }
+    public void onClick4(View view){
+        setContentView(R.layout.five);
+    }
+    public void onClick5(View view){
+        setContentView(R.layout.six);
+    }
+    public void onClick6(View view){
         setContentView(R.layout.seven);
+    }
+    public void onClick7(View view){
+        setContentView(R.layout.one);
     }
 }
